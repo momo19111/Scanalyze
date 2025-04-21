786bb9233ffea3faf716121981674c9ebfcccece
Abdalla Gaber <48131968+abdallagaber@users.noreply.github.com>
2025-04-21 20:17:11 +0200

    Refactor Puppeteer arguments for WhatsApp client initialization to streamline options

--- utils/whatsappClient.js
+++ utils/whatsappClient.js
@@ -11,17 +11,7 @@ const initWhatsapp = async () => {
     puppeteer: {
       headless: true,
       executablePath: "/usr/bin/google-chrome-stable",
-      args: [
-        "--no-sandbox",
-        "--disable-setuid-sandbox",
-        "--disable-dev-shm-usage",
-        "--disable-accelerated-2d-canvas",
-        "--no-first-run",
-        "--no-zygote",
-        "--single-process",
-        "--disable-gpu",
-        "--disable-popup-blocking",
-      ],
+      args: ["--no-sandbox", "--disable-setuid-sandbox"],
     },
   });
 
