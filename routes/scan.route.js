41862eed48f90a5fadaf7fca3266629a8646a4a7
momo19111 <138802969+momo19111@users.noreply.github.com>
2025-05-12 12:51:06 +0300

    scan.route.js

--- routes/scan.route.js
+++ routes/scan.route.js
@@ -4,16 +4,15 @@ const router = express.Router();
 const { getScan, getAllScans, createScan, updateScan, deleteScan, uploadUserImage, resizeImage } = require('../controllers/scan.controller'); 
 const { getScanValidator, createScanValidator, updateScanValidator, deleteScanValidator } = require('../utils/validator/scanValidator') 
 const { protect, allowedTo } = require('../controllers/auth.controller');
-router.use(protect);
 
 router.route('/')
     .get(getAllScans)
-    .post(allowedTo('ScanTechnician', 'Admin'), uploadUserImage, resizeImage, createScanValidator,createScan);
+    .post(protect, allowedTo('ScanTechnician', 'Admin'), uploadUserImage, resizeImage, createScanValidator,createScan);
 
 router.route('/:id')
     .get(getScanValidator, getScan)
-    .put(allowedTo('ScanTechnician', 'Admin'), uploadUserImage, resizeImage, updateScanValidator, updateScan)
-    .delete(allowedTo('ScanTechnician', 'Admin'), deleteScanValidator, deleteScan);
+    .put(protect, allowedTo('ScanTechnician', 'Admin'), uploadUserImage, resizeImage, updateScanValidator, updateScan)
+    .delete(protect, allowedTo('ScanTechnician', 'Admin'), deleteScanValidator, deleteScan);
 
 module.exports = router;
 
