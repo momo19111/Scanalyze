8abf5547963b56e4aed45ae27ca6c3c8e3edf13d
momo19111 <138802969+momo19111@users.noreply.github.com>
2025-05-12 12:50:13 +0300

    labTest.route.js

--- routes/labTest.route.js
+++ routes/labTest.route.js
@@ -4,15 +4,13 @@ const router = express.Router();
 const { getAlllabTests, getlabTest, updatelabTest, deletelabTest, createlabTest } = require('../controllers/labTest.controller'); 
 const { protect, allowedTo } = require('../controllers/auth.controller');
 
-router.use(protect);
-
 router.route('/')
     .get(getAlllabTests)
-    .post(allowedTo('Admin', 'LabTechnician'), createlabTest);
+    .post(protect, allowedTo('Admin', 'LabTechnician'), createlabTest);
 
 router.route('/:id')
     .get(getlabTest)
-    .put(allowedTo('Admin', 'LabTechnician'), updatelabTest)
-    .delete(allowedTo('Admin', 'LabTechnician'), deletelabTest);
+    .put(protect, allowedTo('Admin', 'LabTechnician'), updatelabTest)
+    .delete(protect, allowedTo('Admin', 'LabTechnician'), deletelabTest);
 
 module.exports = router;
